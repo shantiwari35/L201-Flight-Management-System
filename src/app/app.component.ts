da80f34d9602bdfefe0c73774da9a49bcee84ec5
Shantanu Rajesh Tiwari <shantanu.10830303@ltimindtree.com>
2025-03-21 15:59:45 +0530

    create page layotu and added data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from "./ui/footer/footer.component";
+import { NavbarComponent } from "./ui/navbar/navbar.component";
+import { ContentViewComponent } from './ui/content-view/content-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavbarComponent,ContentViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/ui/content-view/content-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-view',
+  imports: [],
+  templateUrl: './content-view.component.html',
+  styleUrl: './content-view.component.css'
+})
+export class ContentViewComponent {
+
+}
